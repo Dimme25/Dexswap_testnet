4ee6536adcccb4f838fc0c0700537f576c80eb86
Dimme25 <dimmewestfingers@gmail.com>
2025-02-13 22:22:38 +0100

    first commit

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -11,7 +11,7 @@ export default function Navbar() {
             justifyContent: "space-between",
             margin: "10px",
         }}>
-            <h1>DEXSWAP</h1>
+            <h1>Swap BNB/NBLA</h1>
             <ConnectWallet />
         </div>
     );
